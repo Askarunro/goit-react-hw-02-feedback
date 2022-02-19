8e67b3b4e554dada65eb5bc37d15913204b0066c
Askarunro <paskar.y.a@gmail.com>
2022-02-19 23:41:46 +0200

    Update Statistics.js

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,98 +1,36 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 // import PropTypes from 'prop-types';
 // import s from './Statistics.module.css';
+import PropTypes from "prop-types";
 
-class Statistics extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-    total: 0,
-    positive: 0,
-  };
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
+  return (
+    <>
+      <p>
+        Good:<span>{good}</span>
+      </p>
+      <p>
+        Neutral:<span>{neutral}</span>
+      </p>
+      <p>
+        Bad:<span>{bad}</span>
+      </p>
+      <p>
+        Total:<span>{total}</span>
+      </p>
+      <p>
+        PositiveFeedback:<span>{positivePercentage}</span>%
+      </p>
+    </>
+  );
+};
 
-
-  countTotalFeedback=()=>{
-    this.setState((countTotal) => {
-        return {
-            total: countTotal.total + 1,
-        };
-      });
-  };
-
-  countPositiveFeedbackPercentage=()=>{
-    this.setState((countPositive) => {
-        return {
-            positive: Math.floor((countPositive.good / countPositive.total )*100)
-        };
-      });
-  }
-
-  clickBtnGood = () => {
-    this.setState((btnGoodState) => {
-      return {
-        good: btnGoodState.good + 1,
-      };
-    });
-    this.countTotalFeedback();
-    this.countPositiveFeedbackPercentage();
-  };
-
-  clickBtnNeutral = () => {
-    this.setState((btnGoodNeutral) => {
-      return {
-        neutral: btnGoodNeutral.neutral + 1,
-      };
-    });
-    this.countTotalFeedback();
-    this.countPositiveFeedbackPercentage();
-
-  };
-
-  clickBtnBad = () => {
-    this.setState((btnGoodBad) => {
-      return {
-        bad: btnGoodBad.bad + 1,
-      };
-    });
-    this.countTotalFeedback();
-    this.countPositiveFeedbackPercentage();
-  };
-
-  render() {
-    return (
-      <div>
-        <div>
-          <button type="button" onClick={this.clickBtnGood} data="good">
-            Good
-          </button>
-          <button type="button" onClick={this.clickBtnNeutral} data="neutral">
-            Neutral
-          </button>
-          <button type="button" onClick={this.clickBtnBad} data="bad">
-            Bad
-          </button>
-        </div>
-
-        <h2>Statistics</h2>
-        <p>
-          Good:<span>{this.state.good}</span>
-        </p>
-        <p>
-          Neutral:<span> {this.state.neutral}</span>
-        </p>
-        <p>
-          Bad:<span> {this.state.bad}</span>
-        </p>
-        <p>
-          Total:<span> {this.state.total}</span>
-        </p>
-        <p>
-          PositiveFeedback:<span>{this.state.positive}</span>
-        %</p>
-      </div>
-    );
-  }
-}
+Statistics.propTypes = {
+  good: PropTypes.number,
+  neutral: PropTypes.number,
+  bad: PropTypes.number,
+  total: PropTypes.number,
+  positivePercentage: PropTypes.number,
+};
 
 export default Statistics;
