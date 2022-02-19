5d4b38aef72add134bf38bc05f20d245e254501c
Askarunro <paskar.y.a@gmail.com>
2022-02-19 23:41:37 +0200

    Update FeedbackOptions.js

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -1,2 +1,19 @@
-import React, {Component} from 'react';
+// import React, {Component} from 'react';
+import React from "react";
 
+const feedbackOptions = ({ onClickGood, onClickNeutral, onClickBad }) => {
+  return (
+    <>
+      <button type="button" onClick={onClickGood} data="good">
+        Good
+      </button>
+      <button type="button" onClick={onClickNeutral} data="neutral">
+        Neutral
+      </button>
+      <button type="button" onClick={onClickBad} data="bad">
+        Bad
+      </button>
+    </>
+  );
+};
+export default feedbackOptions;
